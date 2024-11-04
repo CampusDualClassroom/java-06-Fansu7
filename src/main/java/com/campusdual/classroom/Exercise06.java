8a2a35b48b3f6959eeadd6c36ae7943e4cd09354
Alfonso Vidal <vidaltorres.alfonso@gmail.com>
2024-11-04 18:00:43 +0100

    Se añaden las llaves en los case que faltaban

--- src/main/java/com/campusdual/classroom/Exercise06.java
+++ src/main/java/com/campusdual/classroom/Exercise06.java
@@ -31,27 +31,30 @@ public class Exercise06 {
                 case 3:
                 case 4:
                 case 5:
-                case 6:
+                case 6: {
                     System.out.println("It's night");
                     break;
+                }
                 case 7:
                 case 8:
                 case 9:
                 case 10:
                 case 11:
                 case 12:
-                case 13:
+                case 13: {
                     System.out.println("It's morning");
                     break;
+                }
                 case 14:
                 case 15:
                 case 16:
                 case 17:
                 case 18:
                 case 19:
-                case 20:
+                case 20: {
                     System.out.println("It's afternoon");
                     break;
+                }
                 default:
                     throw new IllegalArgumentException("Unexpected value: " + hour);
             }
